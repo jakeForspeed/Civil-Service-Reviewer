e12401894205b488f593003bdcdea5ed3e9a87dc
Robert Jake Matao <robertjakematao@gmail.com>
2026-07-30 10:52:59 +0800

    Dedug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
-import MockOverview from "./pages/MockOverview";
-import Review from "./pages/Review";
-import Quiz from "./pages/Quiz";
-import Result from "./pages/Result";
-import MockTest from "./pages/MockTest";
-import TestResult from "./pages/TestResult";
+
 
 
 
@@ -22,35 +17,7 @@ function App() {
         element={<MainLayout><Home /></MainLayout>}
       />
 
-      <Route
-        path="/mock-overview"
-        element={<MainLayout><MockOverview /></MainLayout>}
-      />
 
-      <Route
-        path="/review"
-        element={<MainLayout><Review /></MainLayout>}
-      />
-
-      <Route
-        path="/quiz"
-        element={<MainLayout><Quiz /></MainLayout>}
-      />
-      
-      <Route
-        path="/mock-test"
-        element={<MainLayout><MockTest /></MainLayout>}
-      />
-
-      <Route
-        path="/result"
-        element={<MainLayout><Result /></MainLayout>}
-      />
-
-      <Route
-        path="/test-result"
-        element={<MainLayout><TestResult /></MainLayout>}
-      />
 
 
     </Routes>
